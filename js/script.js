42824b7122b34fe55496838b3bbe62c51aa6d4a3
Khuong Nguyen <aknguyen11906@gmail.com>
2018-07-15 14:58:16 +0700

    Test code metrics

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    console.log('Test code metric');
     $(".hamburger").click(function() {
         $("#menu").toggle(function() {
             $(".hamburger").text('☰');
